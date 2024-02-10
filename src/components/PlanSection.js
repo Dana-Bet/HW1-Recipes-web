5b3202367a27db94bfb34af7bf091e633a517eaa
Dana Betesh <75725202+Dana-Bet@users.noreply.github.com>
2024-02-11 00:58:25 +0200

    DONE

--- src/components/PlanSection.js
+++ src/components/PlanSection.js
@@ -6,7 +6,10 @@ import app2 from '../img/recipe_img/appetiser2.png';
 import app3 from '../img/recipe_img/appetiser3.png';
 import star1 from '../img/recipe_img/Arancini.png';
 import star2 from '../img/recipe_img/Pasta salad.png';
-
+import main1 from '../img/recipe_img/Dijon salmon and lemon.png';
+import main2 from '../img/recipe_img/Seasoned chicken.png';
+import dess1 from '../img/recipe_img/Chocolate Souffle.png';
+import dess2 from '../img/recipe_img/Apple pie.png';
 
 // Recipe component
 const Recipe = ({ name, imageUrl }) => {
@@ -50,23 +53,45 @@ export default function PlanSection() {
                     <div className="row">
                         <h2 className="row-title">Main dishes</h2>
                         <div className="dish-container">
-                            <Recipe name="Creamy bread" imageUrl={app1}/>
-                            <Recipe name="Cheese plat" imageUrl={app2}/>
+                            <Recipe name="Dijon salmon&lemon" imageUrl={main1}/>
+                            <Recipe name="Seasoned chicken" imageUrl={main2}/>
                             {/* Add more recipes as needed */}
                        </div>
                     </div>
                     <div className="row">
                         <h2 className="row-title">Desserts</h2>
                         <div className="dish-container">
-                            <Recipe name="Creamy bread" imageUrl={app1}/>
-                            <Recipe name="Cheese plat" imageUrl={app2}/>
+                            <Recipe name="Chocolate Souffle" imageUrl={dess1}/>
+                            <Recipe name="Apple pie" imageUrl={dess2}/>
                             {/* Add more recipes as needed */}
                        </div>
                     </div>
                 </div>
                 <div className="column">
-                    <h2>Your Meal</h2>
-                    <p>table</p>
+                    <h2 className="row-title">Your Meal</h2>
+                    <div className="table-section">
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th>Recipe</th>
+                                    <th>Quantity</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {/* Add selected recipes here */}
+                                <tr>
+                                    <td>Creamy bread</td>
+                                    <td>1</td>
+                                </tr>
+                                <tr>
+                                    <td>Chocolate Souffle</td>
+                                    <td>1</td>
+                                </tr>
+                                {/* Add more rows as needed */}
+                            </tbody>
+                        </table>
+                    </div>
+                    <button className="create-list-button">Create Shopping List</button>
                 </div>
             </div>           
         </div>
