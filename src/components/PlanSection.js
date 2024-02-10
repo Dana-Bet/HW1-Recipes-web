ebbcaca49c684f88ecd720ced48f2758cb5f9445
Dana Betesh <75725202+Dana-Bet@users.noreply.github.com>
2024-02-11 00:22:45 +0200

    dish containers

--- src/components/PlanSection.js
+++ src/components/PlanSection.js
@@ -1,6 +1,20 @@
 import React from 'react';
 import '../styles/PlanSection.css'; 
 import planBg from '../img/planBg.jpg';
+import app1 from '../img/recipe_img/appetiser1.png';
+import app2 from '../img/recipe_img/appetiser2.png';
+import app3 from '../img/recipe_img/appetiser3.png';
+
+// Recipe component
+const Recipe = ({ name, imageUrl }) => {
+    return (
+        <div className="recipe">
+            <h3>{name}</h3>
+            <img src={imageUrl} alt={name} className="recipe-image" />
+            <button className="add-to-meal">Add to the meal</button>
+        </div>
+    );
+};
 
 export default function PlanSection() {
     return (
@@ -14,11 +28,13 @@ export default function PlanSection() {
             <div className="content">
                 <div className="column">
                     <div className="row">
-                    <h2 className="row-title">Appetizers</h2>
-                        <div className="dish-container">
-
+                       <h2 className="row-title">Appetizers</h2>
+                       <div className="dish-container">
+                            <Recipe name="Dish 1" imageUrl={app1}/>
+                            <Recipe name="Dish 1" imageUrl={app2}/>
+                            <Recipe name="Dish 1" imageUrl={app3}/>
                             {/* Add more recipes as needed */}
-                        </div>
+                       </div>
                     </div>
                     <div className="row">
                         <h2 className="row-title">Starters</h2>
