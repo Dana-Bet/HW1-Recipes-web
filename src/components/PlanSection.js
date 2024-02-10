ba22f88c0fb7af8c4109c137cbdc3bf48e4479a4
Dana Betesh <75725202+Dana-Bet@users.noreply.github.com>
2024-02-11 00:36:31 +0200

    with startes

--- src/components/PlanSection.js
+++ src/components/PlanSection.js
@@ -4,6 +4,9 @@ import planBg from '../img/planBg.jpg';
 import app1 from '../img/recipe_img/appetiser1.png';
 import app2 from '../img/recipe_img/appetiser2.png';
 import app3 from '../img/recipe_img/appetiser3.png';
+import star1 from '../img/recipe_img/Arancini.png';
+import star2 from '../img/recipe_img/Pasta salad.png';
+
 
 // Recipe component
 const Recipe = ({ name, imageUrl }) => {
@@ -11,7 +14,7 @@ const Recipe = ({ name, imageUrl }) => {
         <div className="recipe">
             <h3>{name}</h3>
             <img src={imageUrl} alt={name} className="recipe-image" />
-            <button className="add-to-meal">Add to the meal</button>
+            <button className="add-to-meal">+</button>
         </div>
     );
 };
@@ -30,28 +33,40 @@ export default function PlanSection() {
                     <div className="row">
                        <h2 className="row-title">Appetizers</h2>
                        <div className="dish-container">
-                            <Recipe name="Dish 1" imageUrl={app1}/>
-                            <Recipe name="Dish 1" imageUrl={app2}/>
-                            <Recipe name="Dish 1" imageUrl={app3}/>
+                            <Recipe name="Creamy bread" imageUrl={app1}/>
+                            <Recipe name="Cheese plat" imageUrl={app2}/>
+                            <Recipe name="Mini pie" imageUrl={app3}/>
                             {/* Add more recipes as needed */}
                        </div>
                     </div>
                     <div className="row">
                         <h2 className="row-title">Starters</h2>
-                        <p>Content for starters goes here.</p>
+                        <div className="dish-container">
+                            <Recipe name="Arancini" imageUrl={star1}/>
+                            <Recipe name="Pasta salad" imageUrl={star2}/>
+                            {/* Add more recipes as needed */}
+                       </div>
                     </div>
                     <div className="row">
                         <h2 className="row-title">Main dishes</h2>
-                        <p>Content for main dishes goes here.</p>
+                        <div className="dish-container">
+                            <Recipe name="Creamy bread" imageUrl={app1}/>
+                            <Recipe name="Cheese plat" imageUrl={app2}/>
+                            {/* Add more recipes as needed */}
+                       </div>
                     </div>
                     <div className="row">
                         <h2 className="row-title">Desserts</h2>
-                        <p>Content for desserts goes here.</p>
+                        <div className="dish-container">
+                            <Recipe name="Creamy bread" imageUrl={app1}/>
+                            <Recipe name="Cheese plat" imageUrl={app2}/>
+                            {/* Add more recipes as needed */}
+                       </div>
                     </div>
                 </div>
                 <div className="column">
-                    <h2>Column 2</h2>
-                    <p>Content for column 2 goes here.</p>
+                    <h2>Your Meal</h2>
+                    <p>table</p>
                 </div>
             </div>           
         </div>
